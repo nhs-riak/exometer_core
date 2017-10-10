5a3da0d6d6db486aee861d4f8cfee7bc60439463
Russell Brown <russell.brown@mac.com>
2017-10-10 13:23:00 +0100

    Fix failing test with correct tuple entry

--- src/exometer_util.erl
+++ src/exometer_util.erl
@@ -357,7 +357,7 @@ key_match_test() ->
     {ok,yes} = report_type([a,b,c], [], [{[a,b,'_'], yes}]),
     {ok,yes} = report_type([a,b,c], [], [{[a,'_',c], yes}]),
     {ok,yes} = report_type([a,b,c], [], [{[a,b|'_'], yes}]),
-    {ok,yes} = report_type([a,b,c], [{type,yes}], [{[a,b,c], no}]),
+    {ok,yes} = report_type([a,b,c], [{report_type,yes}], [{[a,b,c], no}]),
     ok.
 
 -endif.
